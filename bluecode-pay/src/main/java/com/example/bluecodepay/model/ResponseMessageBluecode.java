4939c4237257d99aa6961b9d0ae6a1ca211fa5c9
ToachDown <shelestvetraggg@Gmail.com>
2021-11-02 14:28:32 +0300

    create facade and autocasting

--- bluecode-pay/src/main/java/com/example/bluecodepay/model/ResponseMessageBluecode.java
+++ bluecode-pay/src/main/java/com/example/bluecodepay/model/ResponseMessageBluecode.java
@@ -1,6 +1,8 @@
 package com.example.bluecodepay.model;
 
+import org.springframework.stereotype.Component;
 import template.model.ResponseMessage;
+
 
 public class ResponseMessageBluecode extends ResponseMessage {
 }
